e1b5e86f5313d9cf15b06476e3f785cc09d7502d
Vitalii Posypenko <posypenkov@yandex.ru>
2022-04-06 00:03:21 +0700

    fix(pagination): завершить пагинацию и исправить формирование URL

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -16,16 +16,19 @@
           >
             <img :src="article.author.image" :alt="article.author.username" />
           </router-link>
-        <div class="info">
-          <router-link
-            :to="{name: 'userProfile', params: {slug: article.author.username}}"
-            class="author"
-          >
-            {{ article.author.username }}
-          </router-link>
-          <span class="date">{{ article.createdAt }}</span>
-        </div>
-        <div class="pull-xs-right">ADD TO FAVORITES</div>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username},
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">ADD TO FAVORITES</div>
         </div>
         <router-link
           :to="{name: 'article', params: {slug: article.slug}}"
@@ -37,7 +40,12 @@
           TAG LIST
         </router-link>
       </div>
-        <mcv-pagination :total="total" :limit="limit" :current-page="currentPage" :url="url"/>
+      <mcv-pagination
+        :total="feed.articlesCount"
+        :limit="limit"
+        :current-page="currentPage"
+        :url="baseUrl"
+      />
     </div>
   </div>
 </template>
@@ -46,6 +54,8 @@
 import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
 import McvPagination from '@/components/Pagination'
+import {limit} from '@/helpers/vars'
+import {stringify, parseUrl} from 'query-string'
 
 export default {
   name: 'McvFeed',
@@ -55,26 +65,50 @@ export default {
       require: true,
     },
   },
-    components: {
-      McvPagination
-    },
-    data() {
-      return {
-          total: 500,
-          limit: 10,
-          currentPage: 8,
-          url: '/tags/dragons'
-      }
-    },
+  components: {
+    McvPagination,
+  },
+  data() {
+    return {
+      limit,
+      url: '/tags/dragons',
+    }
+  },
   computed: {
     ...mapState({
       isLoading: (state) => state.feed.isLoading,
       feed: (state) => state.feed.data,
       error: (state) => state.feed.error,
     }),
+    currentPage() {
+      return Number(this.$route.query.page || '1')
+    },
+    baseUrl() {
+      return this.$route.path
+    },
+    offset() {
+      return this.currentPage * limit - limit
+    },
+  },
+  watch: {
+    currentPage() {
+      this.fetchFeed()
+    },
   },
   mounted() {
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+    this.fetchFeed()
+  },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl)
+      const stringifieldParams = stringify({
+        limit,
+        offset: this.offset,
+        ...parsedUrl.query,
+      })
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifieldParams}`
+      this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams})
+    },
   },
 }
 </script>
