de2fa3a29c2713127034847209b4360a07e261d0
Виталий Посыпенко <v.posypenko@baz-on.ru>
2026-03-26 16:24:23 +0700

    изменение базового URL для API

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import { getItem } from '@/helpers/persistanceStorage'
 
 /* =============== Клиент API ============= */
 
-axios.defaults.baseURL = 'https://api.realworld.io/api'
+axios.defaults.baseURL = 'https://conduit-realworld-example-app.fly.dev/api'
 
 axios.interceptors.request.use((config: AxiosRequestConfig) => {
   const token = getItem<string>('accessToken')
